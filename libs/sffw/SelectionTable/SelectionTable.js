5d03199d16258858bd7d0b9b1c2a9d2462a512fb
SimonB87 <buryans@gmail.com>
2021-09-20 12:26:16 +0200

    CD - repo update

--- libs/sffw/SelectionTable/SelectionTable.js
+++ libs/sffw/SelectionTable/SelectionTable.js
@@ -22,6 +22,7 @@ var sffw;
             var SelectionTableModel = /** @class */ (function () {
                 function SelectionTableModel(params, componentInfo) {
                     var _this = this;
+                    this.isScrollable = ko.observable(false);
                     this.columns = [];
                     this.onRowClick = function (_data, event) {
                         _this.index(event.currentTarget.rowIndex);
@@ -40,6 +41,18 @@ var sffw;
                     this.showSelector = params.ShowSelector;
                     this.selectorClass = params.SelectorClass;
                     this.selectorWidth = params.SelectorWidth;
+                    // bacause binding style via $data.isScrollable ? 'auto' : 'unset' 
+                    // can't handle both bool and observable, unlike setting css
+                    // via $data.isScrollable, which works with both without any problem... grrr
+                    // hence we need to convert whatever to observable here and use 
+                    // $data.isScrollable() in style binding
+                    if (_.isFunction(params.IsScrollable)) {
+                        this.isScrollable = params.IsScrollable;
+                    }
+                    else {
+                        this.isScrollable(params.IsScrollable);
+                    }
+                    this.maxHeight = params.MaxHeight;
                     this.columns = params.Columns;
                 }
                 return SelectionTableModel;
@@ -59,7 +72,7 @@ var sffw;
                     viewModel: {
                         createViewModel: function (params, componentInfo) { return new sffw.components.selectionTable.SelectionTableModel(params, componentInfo); }
                     },
-                    template: "\n<table class=\"sffw-selectiontable alternate-rows hover-highlight\" data-bind=\"visible: $data.isVisible\">\n    <thead>\n        <tr class=\"selectiontable-header-row\">\n            <!-- ko if: $data.showSelector -->\n            <th class=\"selectiontable-column selector container\" data-bind=\"style: { width: $data.selectorWidth }\"></th>\n            <!-- /ko -->\n            <!-- ko foreach: columns -->\n            <th class=\"selectiontable-column container\" data-bind=\"style: { width: $data.ColumnWidth }, visible: $data.IsVisible\">\n                <span data-bind=\"text: $data.Caption\"></span>\n            </th>\n            <!-- /ko -->\n        </tr>\n    </thead>\n    <tbody>\n        <!-- ko foreach: items -->\n        <tr class=\"selectiontable-row\" data-bind=\"click: $parent.onRowClick, css: { selected: $parent.index() == $index() + 1 }\">\n            <!-- ko if: $parent.showSelector -->\n            <td class=\"selector container\">\n                <!-- ko if: $parent.index() == $index() + 1 -->\n                <span data-bind=\"class: $parent.selectorClass\"></span>\n                <!-- /ko -->\n            </td>\n            <!-- /ko -->\n            <!-- ko foreach: $parent.columns -->\n            <td class=\"container\" data-bind=\"visible: $data.IsVisible\">\n                <!-- ko template: { nodes: [$componentTemplateNodes[$index()]], data: $parent } --><!-- /ko -->\n            </td>\n            <!-- /ko -->\n        </tr>\n        <!-- /ko -->\n    </tbody>\n</table>"
+                    template: "\n<div class=\"sffw-selectiontable-scroll\" data-bind=\"style: { 'max-height': $data.isScrollable() ? $data.maxHeight : '', 'overflow-y': $data.isScrollable() ? 'auto' : 'unset' }\">\n    <table class=\"sffw-selectiontable alternate-rows hover-highlight\" data-bind=\"visible: $data.isVisible\">\n        <thead>\n            <tr class=\"selectiontable-header-row\">\n                <!-- ko if: $data.showSelector -->\n                <th class=\"selectiontable-column selector container\" data-bind=\"style: { width: $data.selectorWidth }, css: { 'sticky-table-header': $data.isScrollable }\"></th>\n                <!-- /ko -->\n                <!-- ko foreach: columns -->\n                <th class=\"selectiontable-column container\" data-bind=\"style: { width: $data.ColumnWidth }, visible: $data.IsVisible, css: { 'sticky-table-header': $parent.isScrollable }\">\n                    <span data-bind=\"text: $data.Caption\"></span>\n                </th>\n                <!-- /ko -->\n            </tr>\n        </thead>\n        <tbody>\n            <!-- ko foreach: items -->\n            <tr class=\"selectiontable-row\" data-bind=\"click: $parent.onRowClick, css: { selected: $parent.index() == $index() + 1 }\">\n                <!-- ko if: $parent.showSelector -->\n                <td class=\"selector container\">\n                    <!-- ko if: $parent.index() == $index() + 1 -->\n                    <span data-bind=\"class: $parent.selectorClass\"></span>\n                    <!-- /ko -->\n                </td>\n                <!-- /ko -->\n                <!-- ko foreach: $parent.columns -->\n                <td class=\"container\" data-bind=\"visible: $data.IsVisible\">\n                    <!-- ko template: { nodes: [$componentTemplateNodes[$index()]], data: $parent } --><!-- /ko -->\n                </td>\n                <!-- /ko -->\n            </tr>\n            <!-- /ko -->\n        </tbody>\n    </table>\n</div>"
                 });
             }
         })(selectionTable = components.selectionTable || (components.selectionTable = {}));
--- libs/sffw/SelectionTable/SelectionTableGenerator.js
+++ libs/sffw/SelectionTable/SelectionTableGenerator.js
@@ -70,6 +70,22 @@
 					paramsParts.push('SelectorWidth: \'20px\'');
 				}
 
+				if (typeof def.IsScrollable != 'undefined') {
+					if (def.IsScrollable.Binding) {
+						paramsParts.push('IsScrollable: '+ componentGen.processBinding(def.IsScrollable.Binding));
+					} else {
+						paramsParts.push('IsScrollable: ' + def.IsScrollable);
+					}
+				} else {
+					paramsParts.push('IsScrollable: ' + false);
+				}
+
+				if (def.MaxHeight) {
+					paramsParts.push('MaxHeight: \'' + def.MaxHeight.replace(/\"/g, '&quot;') + '\'');
+				} else {
+					paramsParts.push('MaxHeight: \'200px\'');
+				}
+
                 if (def.Columns) {
 					Columns = _.map(def.Columns, function (c) {
 						var resultParts = [];
@@ -98,6 +114,149 @@
 
 						if (c.Content) {
 							selectionTable.content.push(generateComponentTree(c.Content, isDesignTime));
+						}				
+		
+						return '{' + resultParts.join(', ') + '}';
+					});
+
+					paramsParts.push('Columns: [' + Columns.join(', ') + ']');
+				}
+
+				if (def.CssClass) {
+					selectionTable.cssBinding = selectionTable.cssBinding || {};
+					_.each(def.CssClass, function (c) {
+						if (c.ClassName) {
+							var condition;
+							if (c.Condition === false) {
+								condition =  c.Condition;
+							} else if (c.Condition) {
+								condition = c.Condition.Binding ? processBinding(c.Condition.Binding) : '\'' + c.Condition.replace(/\"/g, '&quot;') + '\'';
+							} else {
+								condition = true;
+							}
+	
+							selectionTable.cssBinding[c.ClassName] = condition;
+						}
+					});
+				}
+					
+				paramsParts.push('$parentData: $data');
+
+				selectionTable.attributes.params = paramsParts.join(', ');
+
+				componentWrapperTree.content.push(selectionTable);
+			}
+		},
+		gen: function (_a) {
+			var cGen = _a.cGen,
+				def = _a.def,
+				componentWrapperTree = _a.componentWrapperTree, 
+				isDesignTime = _a.isDesigntime, 
+				containerEnabled = _a.containerEnabled;
+
+		    var selectionTable, designLabel, paramsParts,
+		        processBinding = cGen.processBinding,
+				genComponentTree = cGen.genComponentTree,
+				Columns;
+
+			if (isDesignTime) {
+				designLabel = new cGen.Tree('span');
+				designLabel.content.push({ text: 'selectionTable' + def.name });
+				componentWrapperTree.content.push(designLabel);
+			} else {
+				selectionTable = new cGen.Tree('sffw-selection-table');
+
+                paramsParts = [];
+
+				if(def.Data && def.Data.Binding) {
+					paramsParts.push('Data: ' + processBinding(def.Data.Binding, null));
+				}
+
+				if (def.Index) {
+					if (def.Index.Binding) {
+						paramsParts.push('Index: '+ cGen.processBinding(def.Index.Binding));
+					} else {
+						paramsParts.push('Index: ' + def.Index);
+					}
+				}
+
+				if (typeof def.IsVisible != 'undefined') {
+					if (def.Visible.Binding) {
+						paramsParts.push('IsVisible: '+ cGen.processBinding(def.IsVisible.Binding));
+					} else {
+						paramsParts.push('IsVisible: ' + def.IsVisible);
+					}
+				} else {
+					paramsParts.push('IsVisible: ' + true);
+				}
+
+				if (typeof def.ShowSelector != 'undefined') {
+					if (def.ShowSelector.Binding) {
+						paramsParts.push('ShowSelector: '+ cGen.processBinding(def.ShowSelector.Binding));
+					} else {
+						paramsParts.push('ShowSelector: ' + def.ShowSelector);
+					}
+				} else {
+					paramsParts.push('ShowSelector: ' + true);
+				}
+
+				if (def.SelectorClass) {
+					paramsParts.push('SelectorClass: \'' + def.SelectorClass.replace(/\"/g, '&quot;') + '\'');
+				} else {
+					paramsParts.push('SelectorClass: \'fa fa-fw fa-lg fa-caret-right\'');
+				}
+
+				if (def.SelectorWidth) {
+					paramsParts.push('SelectorWidth: \'' + def.SelectorWidth.replace(/\"/g, '&quot;') + '\'');
+				} else {
+					paramsParts.push('SelectorWidth: \'20px\'');
+				}
+
+				if (typeof def.IsScrollable != 'undefined') {
+					if (def.IsScrollable.Binding) {
+						paramsParts.push('IsScrollable: '+ cGen.processBinding(def.IsScrollable.Binding));
+					} else {
+						paramsParts.push('IsScrollable: ' + def.IsScrollable);
+					}
+				} else {
+					paramsParts.push('IsScrollable: ' + false);
+				}
+
+				if (def.MaxHeight) {
+					paramsParts.push('MaxHeight: \'' + def.MaxHeight.replace(/\"/g, '&quot;') + '\'');
+				} else {
+					paramsParts.push('MaxHeight: \'200px\'');
+				}
+
+                if (def.Columns) {
+					var childEnabled = cGen.BindingFactory.addParentToFormPropertyBindings(containerEnabled);
+					Columns = _.map(def.Columns, function (c) {
+						var resultParts = [];
+
+						if (c.Caption) {
+							if (c.Caption.Binding) {
+                                resultParts.push('Caption: '+ cGen.processBinding(c.Caption.Binding));
+							} else {
+								resultParts.push('Caption: \'' + c.Caption.replace(/\"/g, '&quot;') + '\'');
+							}
+						}
+
+						if (typeof c.IsVisible != 'undefined') {
+							if (c.IsVisible.Binding) {
+                                resultParts.push('IsVisible: '+ cGen.processBinding(c.IsVisible.Binding));
+							} else {
+								resultParts.push('IsVisible: ' + c.IsVisible);
+							}
+						} else {
+							resultParts.push('IsVisible: ' + true);
+						}
+
+						if (c.ColumnWidth) {
+							resultParts.push('ColumnWidth: \'' + c.ColumnWidth + '\'');
+						}
+
+						if (c.Content) {
+							selectionTable.content.push(genComponentTree({ def: c.Content, isDesignTime: isDesignTime, containerEnabled: childEnabled }));
 						}				
 		
 						return '{' + resultParts.join(', ') + '}';
